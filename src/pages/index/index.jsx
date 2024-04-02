417e229ad1f09cf75712bf9e9834f6cf327bd20b
Loocor <loocor@gmail.com>
2024-04-03 01:26:31 +0800

    [IMP] 实现获取FMS凭证的样本页面

--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -1,16 +1,34 @@
-import {View} from '@tarojs/components'
+import {useState} from "react";
+import {Textarea, View} from '@tarojs/components'
 import {Button} from "@nutui/nutui-react-taro"
+import {getFmsToken} from "@/utils/token";
 import './index.scss'
 
 function Index() {
+  // 定义 token 状态
+  const [token = '', setToken] = useState()
+
+  // 获取 token 字串
+  const getTokens = async () => {
+    try {
+      const resp = await getFmsToken('dc')
+      setToken(resp)
+    } catch (err) {
+      console.error('Failed to get token:', err)
+    }
+  }
+
+  // 渲染页面
   return (
     <View className='container'>
+      <Textarea
+        className='token-string'
+        autoHeight
+        value={token}
+      />
       <View>
-        欢迎使用 NutUI React 开发 Taro 多端项目
-      </View>
-      <View>
-        <Button type='primary'>
-          NutUI React Button
+        <Button type='primary' onClick={getTokens}>
+          Fetch Token
         </Button>
       </View>
     </View>
