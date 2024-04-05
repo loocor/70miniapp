eb3ea3801c118f710376d3b9f81d0810bdb5fc36
Loocor <loocor@gmail.com>
2024-04-05 20:06:34 +0800

    [ADD] 加入课表首页页面

---
+++ src/pages/schedule/data.js
@@ -0,0 +1,184 @@
+export const group = [
+  {
+    dateFull: '2024-04-05',
+    date: '04.05',
+    week: '今天',
+    course: [
+      {
+        name: '瑜伽',
+        time: '08:00-09:00',
+        coach: '张三',
+        image: 'https://picsum.photos/200',
+        status: 0
+      },
+      {
+        name: '瑜伽',
+        time: '09:00-10:00',
+        coach: '李四',
+        image: 'https://picsum.photos/200',
+        status: 0
+      },
+      {
+        name: '瑜伽',
+        time: '10:00-11:00',
+        coach: '王五',
+        image: 'https://picsum.photos/200',
+        status: 0
+      }
+    ]
+  },
+  {
+    dateFull: '2024-04-06',
+    date: '04.06',
+    week: '周六',
+    course: [
+      {
+        name: '瑜伽',
+        time: '08:00-09:00',
+        coach: '张三',
+        image: 'https://picsum.photos/200',
+        status: 1
+      },
+      {
+        name: '瑜伽',
+        time: '09:00-10:00',
+        coach: '李四',
+        image: 'https://picsum.photos/200',
+        status: 3
+      },
+      {
+        name: '瑜伽',
+        time: '10:00-11:00',
+        coach: '王五',
+        image: 'https://picsum.photos/200',
+        status: 0
+      },
+      {
+        name: '瑜伽',
+        time: '11:00-12:00',
+        coach: '麻七',
+        image: 'https://picsum.photos/200',
+        status: 2
+      },
+      {
+        name: '瑜伽',
+        time: '12:00-13:00',
+        coach: '赵六',
+        image: 'https://picsum.photos/200',
+        status: 2
+      },
+      {
+        name: '瑜伽',
+        time: '13:00-14:00',
+        coach: '钱八',
+        image: 'https://picsum.photos/200',
+        status: 2
+      }
+    ]
+  },
+  {
+    dateFull: '2024-04-07',
+    date: '04.07',
+    week: '周日',
+    course: [
+      {
+        name: '瑜伽',
+        time: '08:00-09:00',
+        coach: '张三',
+        image: 'https://picsum.photos/200',
+        status: 0
+      },
+      {
+        name: '瑜伽',
+        time: '09:00-10:00',
+        coach: '李四',
+        image: 'https://picsum.photos/200',
+        status: 3
+      }
+    ]
+  },
+  {
+    dateFull: '2024-04-08',
+    date: '04.08',
+    week: '周一',
+    course: [
+      {
+        name: '瑜伽',
+        time: '08:00-09:00',
+        coach: '张三',
+        image: 'https://picsum.photos/200',
+        status: 1
+      }
+    ]
+  },
+  {
+    dateFull: '2024-04-09',
+    date: '04.09',
+    week: '周二',
+    course: [
+      {
+        name: '瑜伽',
+        time: '08:00-09:00',
+        coach: '张三',
+        image: 'https://picsum.photos/200',
+        status: 1
+      },
+      {
+        name: '瑜伽',
+        time: '09:00-10:00',
+        coach: '李四',
+        image: 'https://picsum.photos/200',
+        status: 0
+      }
+    ]
+  },
+  {
+    dateFull: '2024-04-10',
+    date: '04.10',
+    week: '周三',
+    course: [
+      {
+        name: '瑜伽',
+        time: '08:00-09:00',
+        coach: '张三',
+        image: 'https://picsum.photos/200',
+        status: 1
+      },
+      {
+        name: '瑜伽',
+        time: '09:00-10:00',
+        coach: '李四',
+        image: 'https://picsum.photos/200',
+        status: 0
+      },
+      {
+        name: '瑜伽',
+        time: '10:00-11:00',
+        coach: '王五',
+        image: 'https://picsum.photos/200',
+        status: 0
+      }
+    ]
+  },
+  {
+    dateFull: '2024-04-11',
+    date: '04.11',
+    week: '周三',
+    course: [
+      {
+        name: '瑜伽',
+        time: '08:00-09:00',
+        coach: '张三',
+        image: 'https://picsum.photos/200',
+        status: 1
+      },
+      {
+        name: '瑜伽',
+        time: '09:00-10:00',
+        coach: '李四',
+        image: 'https://picsum.photos/200',
+        status: 0
+      }
+    ]
+  },
+]
--- src/pages/schedule/index.jsx
+++ src/pages/schedule/index.jsx
@@ -1,48 +1,144 @@
-import Taro from "@tarojs/taro";
-import {View} from '@tarojs/components'
-import {Button} from "@nutui/nutui-react-taro"
+import {useRef, useState} from "react";
+import {View} from "@tarojs/components";
+import {Avatar, Cell, SearchBar, Swiper, Tabs, Tag} from "@nutui/nutui-react-taro"
+import {ArrowRight} from "@nutui/icons-react-taro";
+import {group} from "./data";
 import './index.scss'
 
 function Index() {
-  // 跳转页面
-  const gotoSubpage = (id) => {
-    let page = ''
-    switch (id) {
-      case 1:
-        page = '/modules/schedule/group/index'
-        break
-      case 2:
-        page = '/modules/schedule/personal/index'
-        break
-      case 3:
-        page = '/modules/schedule/booked/index'
-        break
-    }
+  // 状态变量
+  const [typeIndex, setTypeIndex] = useState(0)
+  const [dayIndex, setDayIndex] = useState(0)
+  const courseSwiperRef = useRef(0)
 
-    Taro.navigateTo({
-      url: page
-    })
+  // 搜索框
+  const search = (
+    <SearchBar
+      shape='round'
+      placeholder='可按教练、课程、时间搜索'
+      style={{
+        '--nutui-searchbar-input-text-align': 'center',
+      }}
+    />
+  )
+
+  // 类别标签
+  const typeBar = (
+    <Tabs
+      onChange={(type) => {
+        setTypeIndex(type)
+      }}
+      style={{
+        '--nutui-tabs-titles-font-size': '16px',
+        '--nutui-tabs-titles-item-color': '#444',
+      }}
+      activeType='line'
+      value={typeIndex}
+    >
+      <Tabs.TabPane title='团课' />
+      <Tabs.TabPane title='私教' />
+      <Tabs.TabPane title='已约' />
+    </Tabs>
+  )
+
+  // 日期标签
+  const daysSwitcher = (
+    <Tabs
+      onChange={(day) => {
+        courseSwiperRef.current?.to(day)
+        setDayIndex(day)
+      }}
+      activeType='card'
+      value={dayIndex}
+      style={{
+        '--nutui-tabs-titles-font-size': '13px',
+        '--nutui-tabs-titles-item-color': '#888',
+        '--nutui-tabs-titles-item-active-color': '#444',
+        '--nutui-tabs-titles-item-active-font-weight': 'bold',
+      }}
+    >
+      {group.map((item) => (
+        <Tabs.TabPane
+          key={item.dateFull}
+          title={
+            <View className='title'>
+              <View className='date'>{item.date}</View>
+              <View className='week'>{item.week}</View>
+            </View>
+          }
+        />
+      ))}
+    </Tabs>
+  )
+
+  // 课程状态
+  const status = (s) => {
+    switch (s) {
+      case 0:
+        return <Tag round type='info'>可预约</Tag>
+      case 1:
+        return <Tag round type='primary'>已预约</Tag>
+      case 2:
+        return <Tag round type='warning'>已约满</Tag>
+      case 3:
+        return <Tag round type='danger'>已停约</Tag>
+      default:
+        return <Tag round type='default'>-</Tag>
+    }
   }
+
+  // 课程列表
+  const courseSwiper = (
+    <Swiper
+      loop
+      ref={courseSwiperRef}
+      className='course-swiper'
+      height='calc(100vh - 140px)'
+      onChange={(e) => {
+        setDayIndex(e.detail?.current)
+      }}
+    >
+      {group.map((item, day) => (
+        <Swiper.Item key={item.dateFull}>
+          <Cell.Group className='course-day'>
+            {item.course.map((course, row) => (
+              <Cell
+                key={course.time}
+                className='course-item'
+                title={
+                  <View className='title'>
+                    <Avatar className='image' size='large' src={course.image + `?random=` + day + row} />
+                    <View>
+                      <View className='name'>{course.name}</View>
+                      <View className='time'>{course.time}</View>
+                      <View className='coach'>{course.coach}</View>
+                    </View>
+                  </View>
+                }
+                extra={
+                  <View className='extra'>
+                    {status(course.status)}
+                    <ArrowRight />
+                  </View>
+                }
+              />
+            ))}
+          </Cell.Group>
+        </Swiper.Item>
+      ))}
+    </Swiper>
+  )
 
   // 渲染页面
   return (
-    <View className='container'>
-      <View>
-        <Button type='primary' onClick={() => gotoSubpage(1)}>
-          Group Training
-        </Button>
+    <>
+      {search}
+      {typeBar}
+      <View className='course'>
+        {daysSwitcher}
+        {courseSwiper}
       </View>
-      <View>
-        <Button type='primary' onClick={() => gotoSubpage(2)}>
-          Personal Training
-        </Button>
-      </View>
-      <View>
-        <Button type='primary' onClick={() => gotoSubpage(3)}>
-          Booked Training
-        </Button>
-      </View>
-    </View>
+    </>
   )
 }
 
